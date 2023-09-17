8f32bc57a4a25d6e1304840ea2a7b0a231ae6eab
0ceanSlim <89587889+0ceanSlim@users.noreply.github.com>
2023-09-17 08:48:02 -0400

    added purge back, removing removed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: [
+    './src/**/*.{svelte,html,js,ts}',
+  ],
   content: ['.src/**/*.{svelte,html,js,ts}'],
   theme: {
     extend: {},
